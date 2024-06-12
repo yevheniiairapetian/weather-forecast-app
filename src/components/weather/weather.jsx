fd3dca7370a7c37c4b55b8361bbb9f4475a574de
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-06-13 01:52:07 +0200

    feat: add conditional to card
    Add conditional for changing bg color of card depending on current time

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -70,14 +70,14 @@ const Weather = () => {
 
 
 
-            <Card.Body>
+            <Card.Body className={isDayTime? "card-body moving-background-light":"card-body moving-background-dark"}>
 
               <Card.Title id="card-title" className="item-title text-center fs-6 pb-3 pt-3"><h2 className="weather-city">{weatherData.name}
                 <DisplayDate />
               </h2>
               </Card.Title>
 
-              <Card.Title className="temp-info-container text-center pb-1" ><p className="temperature-info">{weatherData.main.temp}°C</p>
+              <Card.Title className="temp-info-container text-center pb-1" ><p className="temperature-info" style={{color:"whitesmoke"}}>{weatherData.main.temp}°C</p>
 
                 {(weatherData.weather[0].description === 'clear sky' && isDayTime) &&
                   <Card.Img className='w-100 card-image' variant='top'
@@ -205,11 +205,11 @@ const Weather = () => {
                   />
                 }
               </Card.Title>
-              <Card.Title className="item-info text-center pb-1" ><p className="sky-info">{weatherData.weather[0].description}</p></Card.Title>
-              <Card.Title className="item-info text-center pb-1" ><p className="feels-like-info">Feels like : {weatherData.main.feels_like}°C</p></Card.Title>
-              <Card.Title className="item-info text-center pb-1" ><p className="humidity-info">Humidity : {weatherData.main.humidity}%</p></Card.Title>
-              <Card.Title className="item-info text-center pb-1" ><p className="pressure-info">Pressure : {weatherData.main.pressure}</p></Card.Title>
-              <Card.Title className="item-info text-center pb-1" ><p className="wind-speed-info">Wind Speed : {weatherData.wind.speed}m/s</p></Card.Title>
+              <Card.Title className="item-info text-center pb-1" ><p className="sky-info" style={{color:"whitesmoke"}}>{weatherData.weather[0].description}</p></Card.Title>
+              <Card.Title className="item-info text-center pb-1" ><p className="feels-like-info" style={{color:"whitesmoke"}}>Feels like : {weatherData.main.feels_like}°C</p></Card.Title>
+              <Card.Title className="item-info text-center pb-1" ><p className="humidity-info" style={{color:"whitesmoke"}}>Humidity : {weatherData.main.humidity}%</p></Card.Title>
+              <Card.Title className="item-info text-center pb-1" ><p className="pressure-info" style={{color:"whitesmoke"}}>Pressure : {weatherData.main.pressure}</p></Card.Title>
+              <Card.Title className="item-info text-center pb-1" ><p className="wind-speed-info" style={{color:"whitesmoke"}}>Wind Speed : {weatherData.wind.speed}m/s</p></Card.Title>
 
 
             </Card.Body>
