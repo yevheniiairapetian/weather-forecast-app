0a2c19279089c4df2df681ee07518a76cc952ddc
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-06-15 01:40:48 +0200

    feat: update footer text

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ export const Footer = ()=>{
   return(
     <div className='footer'>
       <p className='footer-info-title'>Better Wetter App, 2024</p>
-      <p className='footer-info-copy'>All right reserved &copy;</p>
+      <p className='footer-info-copy'>All rights reserved &copy;</p>
     </div>
   )
 }
