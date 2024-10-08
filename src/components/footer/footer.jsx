badefc5a8ca2a9fac20cf9d8c30f354603aa6534
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-10-08 23:26:16 +0200

    feat: add preloader

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,6 +5,7 @@ export const Footer = ()=>{
     <div className='footer'>
       <p className='footer-info-title'><span className='footer-app-name'>Better Wetter App</span></p>
       <p className='footer-info-copy'>All rights reserved, 2024 &copy;</p>
+      <p>Powered by <a className="powered-by-link" href="https://www.weatherapi.com/" title="Weather API" target="_blank">WeatherAPI.com</a></p>
     </div>
   )
 }
