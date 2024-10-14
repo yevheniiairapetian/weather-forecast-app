602841e79e9384dcf31f29df695ac25e4f3a1a63
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-10-15 01:05:21 +0200

    feat: update cookie dialog text

--- src/components/cookie-consent/cookie-consent.jsx
+++ src/components/cookie-consent/cookie-consent.jsx
@@ -12,7 +12,7 @@ export const CookieComponent = () =>{
         buttonText="OK"
         >
     
-          <span>This app uses cookies to enhance your experience. By continuing to use this app, you are consenting to our use of cookies. Learn more about the </span> 
+          <span>This app uses cookies to make your experience while using it more comfortable. This applies to features including (but not limited to) setting a theme color or language preferences. By continuing to use this app, you are consenting to our use of cookies. Learn more about the </span> 
           <a target="_blank" href="https://www.freeprivacypolicy.com/live/1fa49d34-d4c5-4a16-8198-5018fff05d3b">Cookie</a>
           <span> and </span> 
           <a target="_blank" href="https://www.freeprivacypolicy.com/live/ff5ed076-848c-4497-9332-a7f2f0c2c251">Privacy </a>
